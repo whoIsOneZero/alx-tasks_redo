d4d52bb0938fb67c768e2cb9f64701b15bed97b8
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-08-29 09:51:45 -0700

    doc: frees a linked list

---
+++ alx-low_level_programming/0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - frees a linked list from memory
+ * @head: pointer to the head of the linked list
+ */
+void free_list(list_t *head)
+{
+	list_t *temp; /*temporarily store next node's address...*/
+	/*... while deallocating memory for the current one*/
+
+	/*Traverse the list until head points to null*/
+	while (head != NULL)
+	{
+		temp = head->next; /*Store next node's address*/
+		free(head->str); /*Free data portion of the node*/
+		free(head); /*Free e the current node iteself*/
+		head = temp; /*Head now points to the next node*/
+	}
+}
