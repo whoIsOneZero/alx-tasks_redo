63c7883c771a378c336000d31f5d145dab6e6702
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-08-29 08:16:23 -0700

    doc: adds a new node at the beginning of a linked list

---
+++ alx-low_level_programming/0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * add_node - adds a new node at the beginnig of a list...
+ * ... of type 'list_t'
+ * @head: double pointer to the head of the linked list
+ * @str: new node to be added
+ *
+ * Return: pointer to the new node (SUCCESS)
+ * or NULL (FAIL)
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new; /*pointer to the new node*/
+	unsigned int len = 0;
+
+	while (str[len])
+		len++; /*To get the string length*/
+
+	new = malloc(sizeof(list_t));
+	if (!new) /*Mem alloc. failed. Insufficient memory*/
+		return (NULL);
+
+	/*Allocate new mem. block and copy string*/
+	new->str = strdup(str);
+
+	new->len = len;
+
+	/*'next' of new node should point to old head*/
+	new->next = (*head);
+
+	/*Change head to now point to 'new' node*/
+	(*head) = new;
+
+	return (*head);
+}
