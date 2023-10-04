bd5060f201cf1174119d3bc296f6a847b853ea63
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-10-04 10:54:35 -0700

    doc: task 1

--- alx-higher_level_programming/0x01-python-if_else_loops_functions/1-last_digit.py
+++ alx-higher_level_programming/0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,11 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 
-#number = str(number)  #get the string representation
-#last_dig = number[-1]  #get the last digit
-#num = int(last_dig)  #type cast to int
+# number = str(number)  #get the string representation
+# last_dig = number[-1]  #get the last digit
+# num = int(last_dig)  #type cast to int
 
-last_digit = number % 10   #get the last digit directly
+last_digit = abs(number) % 10   # get the last digit directly
+if (number < 0):
+    last_digit = -last_digit
 
 if (last_digit > 5):
     result = "and is greater than 5"
