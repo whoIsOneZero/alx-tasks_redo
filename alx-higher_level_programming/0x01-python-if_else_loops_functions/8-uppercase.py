8213eb5810e97d4086bd91ddc8346a17552440d4
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-10-06 08:34:39 +0000

    doc: task 8

--- alx-higher_level_programming/0x01-python-if_else_loops_functions/8-uppercase.py
+++ alx-higher_level_programming/0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,12 @@
-#!/usr/bin/python3
+# !/usr/bin/python3
+
+# Prints a string in UPPERCASE
+def uppercase(str):
+    for alpha in str:
+        value = ord(alpha)  # get int unicode
+        if (value > 96 and value < 123):
+            # change to uppercase here
+            alpha = chr(value - 32)
+            
+        print("{}".format(alpha), end="")  # print all UPPERCASE
+    print("")
