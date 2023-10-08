2e3c784bf89cd54bffa1074cf8e9bc6e1b261bbd
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-10-08 17:04:25 +0000

    initial commit

--- alx-higher_level_programming/0x00-python-hello_world/10-check_cycle.c
+++ alx-higher_level_programming/0x00-python-hello_world/10-check_cycle.c
@@ -12,4 +12,21 @@ int check_cycle(listint_t *list)
 {
     listint_t *slow_ptr = list;
     listint_t *fast_ptr = list;
+
+    while(slow_ptr != NULL && fast_ptr != NULL && fast_ptr->next != NULL)
+    {
+        slow_ptr = slow_ptr->next; /*Move by one step*/
+        fast_ptr = fast_ptr->next->next; /*Move by two steps*/
+
+        /*If the two pointers meet, then there's a cycle*/
+        if (slow_ptr == fast_ptr)
+        {
+            return (1);
+        }
+    }
+
+    /*Out of the loop, meaning end of list reached without a meeting point*/
+    /*Hence, there's no cycle*/
+
+    return (0);
 }
