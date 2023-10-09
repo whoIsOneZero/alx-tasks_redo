57fe451ff49f0c2be2908b6d3e656963ed4f5431
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-10-09 10:18:14 -0700

    doc: betty compliant

--- alx-higher_level_programming/0x00-python-hello_world/10-check_cycle.c
+++ alx-higher_level_programming/0x00-python-hello_world/10-check_cycle.c
@@ -3,30 +3,30 @@
 /**
  * check_cycle - checks to  see if there's a loop in the linked list
  * Uses the 'tortoise and hare' algorithm (Floyd's cycle detection)
- * 
+ *
  * @list: a pointer to the linked list
- * 
- * Return: int 
+ *
+ * Return: int
  */
 int check_cycle(listint_t *list)
 {
-    listint_t *slow_ptr = list;
-    listint_t *fast_ptr = list;
+	listint_t *slow_ptr = list;
+	listint_t *fast_ptr = list;
 
-    while(slow_ptr != NULL && fast_ptr != NULL && fast_ptr->next != NULL)
-    {
-        slow_ptr = slow_ptr->next; /*Move by one step*/
-        fast_ptr = fast_ptr->next->next; /*Move by two steps*/
+	while (slow_ptr != NULL && fast_ptr != NULL && fast_ptr->next != NULL)
+	{
+		slow_ptr = slow_ptr->next; /*Move by one step*/
+		fast_ptr = fast_ptr->next->next; /*Move by two steps*/
 
-        /*If the two pointers meet, then there's a cycle*/
-        if (slow_ptr == fast_ptr)
-        {
-            return (1);
-        }
-    }
+		/*If the two pointers meet, then there's a cycle*/
+		if (slow_ptr == fast_ptr)
+		{
+			return (1);
+		}
+	}
 
-    /*Out of the loop, meaning end of list reached without a meeting point*/
+/*Out of the loop, meaning end of list reached without a meeting point*/
     /*Hence, there's no cycle*/
 
-    return (0);
+	return (0);
 }
