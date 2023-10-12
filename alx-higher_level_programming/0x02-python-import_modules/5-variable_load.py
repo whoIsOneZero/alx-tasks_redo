a9ddbc3b5c95fa61a80624f86bc744097dc06de8
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-10-12 11:49:25 -0700

    docs: task 5

--- alx-higher_level_programming/0x02-python-import_modules/5-variable_load.py
+++ alx-higher_level_programming/0x02-python-import_modules/5-variable_load.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    ''' imports the variable a from variable_load_5.py and prints its value.'''
+
+    from variable_load_5 import a
+
+    print("{}".format(a))
