3f82136184693bf89667221ee66ecfb2fd8abe0a
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-10-12 11:35:32 -0700

    docs: task 4

--- alx-higher_level_programming/0x02-python-import_modules/4-hidden_discovery.py
+++ alx-higher_level_programming/0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+" prints all the names defines by a compiled module "
+
+
+if __name__ == "__main__":
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
