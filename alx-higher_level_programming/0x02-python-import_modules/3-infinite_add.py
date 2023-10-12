84e0f7e4938d9c95ee74c28f71b4f090cf002d2d
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-10-12 18:03:22 +0000

    docs: task 3

--- alx-higher_level_programming/0x02-python-import_modules/3-infinite_add.py
+++ alx-higher_level_programming/0x02-python-import_modules/3-infinite_add.py
@@ -3,5 +3,14 @@
 ''' Prints the result of the addition of all arguments '''
 
 
-if __name__ == "__main__"
+if __name__ == "__main__":
+	import sys
+	
+	total = 0
+	#for i in range(len(sys.argv) - 1):
+	#	total += int(sys.argv[i + 1])
 
+	for arg in sys.argv[1:]:
+		total += int(arg)
+
+	print("{}".format(total))
