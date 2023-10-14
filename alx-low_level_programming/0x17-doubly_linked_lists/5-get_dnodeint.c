dcb12604c1c10782ac64d1cbb378e21d9bf912da
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-10-14 22:31:41 +0000

    docs: task 5

--- alx-low_level_programming/0x17-doubly_linked_lists/5-get_dnodeint.c
+++ alx-low_level_programming/0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -4,12 +4,30 @@
  * get_dnodeint_at_index - retuns the nth node of a dlistint_t linked list
  * @head: a pointer to the head of the linked list
  * @index: index of the node to be gotten
- * 
+ *
  * Return: [SUCCESS]: a pointer to the node, [NODE DOESN'T EXIT]: NULL
- * 
+ *
  */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    //
+    unsigned int position = 0;
+
+	if (head == NULL)   /*Empty linked list*/
+		return (NULL);
+
+	while (head->prev != NULL)
+		head = head->prev;   /* Go to the beginning */
+
+	while (head != NULL)
+	{
+		if (position == index)
+			break;
+
+		head = head->next;
+		position++;
+	}
+	/* head == NULL*/
+
+	return (head);
 }
