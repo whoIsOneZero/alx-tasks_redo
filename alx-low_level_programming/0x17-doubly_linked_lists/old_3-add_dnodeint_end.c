9ea40566d6f0e8bf558422bef22414da7835e8ee
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-10-14 11:48:35 +0000

    docs: fixed valgrind errors

--- alx-low_level_programming/0x17-doubly_linked_lists/old_3-add_dnodeint_end.c
+++ alx-low_level_programming/0x17-doubly_linked_lists/old_3-add_dnodeint_end.c
@@ -19,7 +19,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	/* Initialize new node */
 	new->n = n;
-	new->next = NULL; /* fixed the valgrind errors */
 	new->prev = NULL;
 
 	if (ptr == NULL) /*Empty linked list*/
