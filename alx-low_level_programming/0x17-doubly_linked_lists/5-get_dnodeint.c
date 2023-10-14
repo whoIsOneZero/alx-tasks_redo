b05dc9bda1a2c5b9129d05af1cd81cf1142488f2
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-10-14 22:01:04 +0000

    docs: task 5

---
+++ alx-low_level_programming/0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - retuns the nth node of a dlistint_t linked list
+ * @head: a pointer to the head of the linked list
+ * @index: index of the node to be gotten
+ * 
+ * Return: [SUCCESS]: a pointer to the node, [NODE DOESN'T EXIT]: NULL
+ * 
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+    //
+}
