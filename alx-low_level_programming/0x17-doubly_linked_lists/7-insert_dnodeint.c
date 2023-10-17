4dde8f1e4e4d6aa202f321551cf103273517eed3
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-10-17 20:15:37 +0000

    docs: inserts a new node at a given position

---
+++ alx-low_level_programming/0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index -  inserts a new node at a given position
+ * @h: a pointer to the h of the linked list
+ * @idx: index of the node to be gotten
+ *
+ * Return: [SUCCESS]: address of the new node, [CANNOT ADD]: NULL
+ *
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int position = 0;
+	dlistint_t *ptr = h; /*For traversing the list*/
+	dlistint_t temp = NULL;
+	dlistint_t new = NULL;
+
+	if (h == NULL)   /*Empty linked list*/
+		return (NULL);
+
+	while (h->prev != NULL)
+		h = h->prev;   /* Go to the beginning */
+
+	while (position != idx)
+	{
+		ptr = ptr->next;
+		position++;
+	}
+	/* positon == idx*/
+	temp = ptr->next;  /*Node just after the  desired index*/
+	ptr->next = new;
+	temp->prev = new;
+
+	new->next = temp;
+	new->prev = ptr;
+
+	return (h);
+}
