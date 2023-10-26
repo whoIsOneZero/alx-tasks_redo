d2db65f5f4c9dca5e7b454c17cc416811de8be44
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-10-26 13:59:50 +0000

    docs: task 1

---
+++ alx-higher_level_programming/0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" Define a class 'Square' """
+
+
+class Square:
+    """ Definition of a Squre."""
+
+    def __init__(self, size):
+        """ Initialize a Square instance.
+
+        Args:
+            size (int): The size o the new Square.
+        """
+        self.__size = size
