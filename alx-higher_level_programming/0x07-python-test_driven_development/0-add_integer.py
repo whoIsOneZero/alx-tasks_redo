1c6e68b964daf3cc1f77119df0a47c80cedd3ca4
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-11-05 20:33:25 +0000

    docs: task 0

---
+++ alx-higher_level_programming/0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+def add_integer(a, b=98):
+    """Return the integer sum of `a` and `b`
+
+    Floats are typecasted to int before addition is performed.
+
+    Raises:
+        TypeError: `a` or `b` is not an int or float.
+    """
+    if ((not isinstance(a, int) and not isinstant(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstant(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b)
