7d10584e469810199ea2aa9945088c9e050d74eb
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-11-12 17:31:14 +0000

    docs: task 1

---
+++ alx-higher_level_programming/0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+contains MyList class
+"""
+class MyList(list):
+    """A subclass of list"""
+    def __init__(self):
+        """Initializes the object"""
+        super().__init__()
+        
+    def print_sorted(self):
+        """Prints the sorted list"""
+        print(sorted(self))
