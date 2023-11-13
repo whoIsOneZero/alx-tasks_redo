618adae5051679bede5f08e7e4804c8d83896106
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-11-13 22:01:43 +0000

    docs: task 6

---
+++ alx-higher_level_programming/0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Representation of a BaseGeometry"""
+    
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
