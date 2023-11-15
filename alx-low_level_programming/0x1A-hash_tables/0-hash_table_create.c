3d70b4474025c31f311cf2d44e6390ba710f9a10
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-11-15 09:44:57 -0800

    docs: remove hash table create_item()

--- alx-low_level_programming/0x1A-hash_tables/0-hash_table_create.c
+++ alx-low_level_programming/0x1A-hash_tables/0-hash_table_create.c
@@ -1,31 +1,5 @@
 #include "hash_tables.h"
 
-/**
- * create_item - Create new hash table item.
- *
- * @key: the key of the item
- * @value: the value of the item
- * Return: a pointer to the created item
- */
-hash_node_t *create_item(char *key, char *value)
-{
-	hash_node_t *item = (hash_node_t *) malloc(sizeof(hash_node_t));
-
-	if (item == NULL)
-		return (NULL);
-
-	item->key = (char *) malloc(strlen(key) + 1);
-	if (item->key == NULL)
-		return (NULL);
-	item->value = (char *) malloc(strlen(value) + 1);
-	if (item->key == NULL)
-		return (NULL);
-
-	strcpy(item->key, key);
-	strcpy(item->value, value);
-
-	return (item);
-}
 
 /**
  * hash_table_create - creates a hash table
@@ -42,14 +16,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t *));
+	table->array = (hash_node_t **) malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
-	{
-		free(table); /*Free previously allocated memory by `malloc`*/
 		return (NULL);
-	}
 
-	for (i = 0; i < table->size; i++)
+	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 
 	return (table);
