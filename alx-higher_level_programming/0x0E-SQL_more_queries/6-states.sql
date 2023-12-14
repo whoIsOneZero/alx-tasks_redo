e7420452714f766e54b67320bcd4fbf1597c48cb
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-12-14 15:47:42 +0000

    create databse and tabe, auto generated id

--- alx-higher_level_programming/0x0E-SQL_more_queries/6-states.sql
+++ alx-higher_level_programming/0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- Create databse and table
 -- id: auto generated, can't be null and is primary key
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
     id INT UNIQUE NOT NULL AUTO_INCREMENT,
     name VARCHAR(256)
