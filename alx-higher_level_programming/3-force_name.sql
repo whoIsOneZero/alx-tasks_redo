0f9f5a22c77b7f54586f9ad6afe861288efb6a1e
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-12-14 15:48:10 +0000

    remove from wrong directory

--- alx-higher_level_programming/3-force_name.sql
+++
@@ -1,6 +0,0 @@
--- Create table on MySQL server
--- db name passed as an arg of the `mysql` command
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT,
-    name VARCHAR(256) NOT NULL
-    );
--- alx-higher_level_programming/4-never_empty.sql
+++
@@ -1,6 +0,0 @@
--- Create tables
--- db passed as an arg to the `mysql` command
-CREATE TABLE IF NOT EXISTS id_not_null (
-    id INT DEFAULT "1",
-    name VARCHAR(256)
-    );
